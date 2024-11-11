d0f5cd7b0a883101949d3b3981e3bf85b093fe64
sss2013 <kshi9478@gmail.com>
2024-11-11 14:17:24 +0900

    초기 커밋.

---
+++ G24W11WPFCounter/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace G24W11WPFCounter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static int count = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OnAdd(object sender, RoutedEventArgs e)
+        {
+            TextCount.Text = $"{++count}";
+        }
+        private void OnSub(object sender, RoutedEventArgs e)
+        {
+            if (count > 0)
+            {
+                TextCount.Text = $"{--count}";
+            }
+        }
+     }
+}
